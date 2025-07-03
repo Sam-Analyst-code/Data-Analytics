baabd7a6cfd88002e213785f83d28c8d332edf2b
Samuel Mati <samuel.mati76@gmail.com>
2025-07-03 12:48:53 +0300

    Add files via upload

---
+++ PYTHON/logical.py
@@ -0,0 +1,10 @@
+# Get yourr total expenses per month
+rent= 5000
+wifi=1500
+miscellaneous= 3500
+food= 3000
+savings=4500
+
+total_expenses= rent + wife + miscellaneous + food + savings
+
+print("My total expenses this month was", total_expenses)
